fe71c2ec1e474cbaa8ca40e898feb119fc2ca552
ltfide <lutfidendians@gmail.com>
2023-03-04 06:48:46 +0700

    ZonedDateTime Class

--- date-time/src/test/java/belajar/date/time/ZoneDateTimeTest.java
+++ date-time/src/test/java/belajar/date/time/ZoneDateTimeTest.java
@@ -16,10 +16,10 @@ public class ZoneDateTimeTest {
       ZonedDateTime zonedDateTime3 = ZonedDateTime.now(ZoneId.of("GMT"));
       ZonedDateTime zonedDateTime4 = ZonedDateTime.now(ZoneOffset.ofHours(5));
 
-      System.out.println(zonedDateTime1);
-      System.out.println(zonedDateTime2);
-      System.out.println(zonedDateTime3);
-      System.out.println(zonedDateTime4);
+      System.out.println(zonedDateTime1); // 2023-03-04T06:46:29.408771200+07:00[Asia/Jakarta]
+      System.out.println(zonedDateTime2); // 2023-03-04T06:46:29.410771300+07:00[Asia/Jakarta]
+      System.out.println(zonedDateTime3); // 2023-03-03T23:46:29.410771300Z[GMT]
+      System.out.println(zonedDateTime4); // 2023-03-04T04:46:29.410771300+05:00
    }
 
    @Test
@@ -27,20 +27,20 @@ public class ZoneDateTimeTest {
       ZonedDateTime zonedDateTime1 = ZonedDateTime.parse("2022-10-11T12:47:47.561633200+07:00[Asia/Jakarta]");
       ZonedDateTime zonedDateTime2 = ZonedDateTime.parse("2022-10-11T10:47:47.562635+05:00");
 
-      System.out.println(zonedDateTime1);
-      System.out.println(zonedDateTime2);
+      System.out.println(zonedDateTime1); // 2022-10-11T12:47:47.561633200+07:00[Asia/Jakarta]
+      System.out.println(zonedDateTime2); // 2022-10-11T10:47:47.562635+05:00
    }
 
    @Test
    void changeZoneId() {
       ZonedDateTime zonedDateTime1 = ZonedDateTime.now();
-      System.out.println(zonedDateTime1);
+      System.out.println(zonedDateTime1); // 2023-03-04T06:47:13.264524900+07:00[Asia/Jakarta]
 
       ZonedDateTime zonedDateTime2 = zonedDateTime1.withZoneSameLocal(ZoneId.of("GMT"));
       ZonedDateTime zonedDateTime3 = zonedDateTime1.withZoneSameInstant(ZoneId.of("GMT"));
 
-      System.out.println(zonedDateTime2);
-      System.out.println(zonedDateTime3);
+      System.out.println(zonedDateTime2); // 2023-03-04T06:47:13.264524900Z[GMT]
+      System.out.println(zonedDateTime3); // 2023-03-03T23:47:13.264524900Z[GMT]
    }
 
 }
