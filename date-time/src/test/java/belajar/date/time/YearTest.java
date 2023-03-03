437632f8736abb767fcc2f2bd36f8a44ac648229
ltfide <lutfidendians@gmail.com>
2023-03-04 06:31:04 +0700

    Year, YearMonth and MonthDay Class

--- date-time/src/test/java/belajar/date/time/YearTest.java
+++ date-time/src/test/java/belajar/date/time/YearTest.java
@@ -9,44 +9,44 @@ import java.time.YearMonth;
 import org.junit.jupiter.api.Test;
 
 public class YearTest {
-   
+
    @Test
    void create() {
       Year year1 = Year.now();
       Year year2 = Year.of(1998);
       Year year3 = Year.parse("2020");
 
-      System.out.println(year1);
-      System.out.println(year2);
-      System.out.println(year3);
+      System.out.println(year1); // 2023
+      System.out.println(year2); // 1998
+      System.out.println(year3); // 2020
 
       YearMonth yearMonth1 = YearMonth.now();
       YearMonth yearMonth2 = YearMonth.of(1990, Month.MARCH);
       YearMonth yearMonth3 = YearMonth.parse("2020-09");
 
-      System.out.println(yearMonth1);
-      System.out.println(yearMonth2);
-      System.out.println(yearMonth3);
+      System.out.println(yearMonth1); // 2023-03
+      System.out.println(yearMonth2); // 1990-03
+      System.out.println(yearMonth3); // 2020-09
 
       MonthDay monthDay1 = MonthDay.now();
       MonthDay monthDay2 = MonthDay.of(05, 11);
       MonthDay monthDay3 = MonthDay.parse("--10-09");
 
-      System.out.println(monthDay1);
-      System.out.println(monthDay2);
-      System.out.println(monthDay3);
+      System.out.println(monthDay1); // --03-04
+      System.out.println(monthDay2); // --05-11
+      System.out.println(monthDay3); // --10-09
    }
 
    @Test
    void name() {
       Year year = Year.of(1998);
-      System.out.println(year);
+      System.out.println(year); // 1998
 
       LocalDate localDate = year.atMonth(Month.MAY).atDay(06);
-      System.out.println(localDate);
+      System.out.println(localDate); // 1998-05-06
 
       MonthDay monthDay = MonthDay.from(localDate);
-      System.out.println(monthDay);
+      System.out.println(monthDay); // --05-06
    }
 
    @Test
@@ -55,11 +55,11 @@ public class YearTest {
       YearMonth yearMonth = YearMonth.now();
       MonthDay monthDay = MonthDay.now();
 
-      System.out.println(year.getValue());
-      System.out.println(yearMonth.getYear());
-      System.out.println(yearMonth.getMonth());
-      System.out.println(monthDay.getMonth());
-      System.out.println(monthDay.getDayOfMonth());
+      System.out.println(year.getValue()); // 2023
+      System.out.println(yearMonth.getYear()); // 2023
+      System.out.println(yearMonth.getMonth()); // MARCH
+      System.out.println(monthDay.getMonth()); // MARCH
+      System.out.println(monthDay.getDayOfMonth()); // 4
    }
 
 }
