debc7ad99a192c9cca4f34046cc4822a0087516b
ltfide <lutfidendians@gmail.com>
2023-03-04 06:30:26 +0700

    TimeZone Class

--- date-time/src/test/java/belajar/date/time/TimeZoneTest.java
+++ date-time/src/test/java/belajar/date/time/TimeZoneTest.java
@@ -12,21 +12,23 @@ public class TimeZoneTest {
    @Test
    void testTimeZone() {
       Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Jakarta"));
-      System.out.println(calendar.get(Calendar.HOUR_OF_DAY));
+      System.out.println(calendar.get(Calendar.HOUR_OF_DAY)); // 6
 
       TimeZone timeZone = TimeZone.getDefault();
+      // sun.util.calendar.ZoneInfo[id="Asia/Jakarta",offset=25200000,dstSavings=0,useDaylight=false,transitions=9,lastRule=null]
       System.out.println(timeZone);
 
       String[] availableIDs = TimeZone.getAvailableIDs();
-      Arrays.asList(availableIDs).forEach(zoneId -> System.out.println(zoneId));
+      Arrays.asList(availableIDs).forEach(zoneId -> System.out.println(zoneId)); // list time zone
    }
 
-   @Test 
+   @Test
    void testDate() {
       Date date = new Date();
 
-      System.out.println(date.toString());
-      // System.out.println(date.toGMTString()); // Deprecated
+      System.out.println(date.toString()); // Fri Mar 03 06:47:21 WIB 2023
+      // Deprecated
+      System.out.println(date.toGMTString()); // 2 Mar 2023 23:47:21 GMT
    }
 
    @Test
@@ -34,11 +36,11 @@ public class TimeZoneTest {
       Calendar calendarJakarta = Calendar.getInstance(TimeZone.getTimeZone("Asia/Jakarta"));
       Calendar calendarGMT = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
 
-      System.out.println(calendarJakarta.get(Calendar.HOUR_OF_DAY));
-      System.out.println(calendarGMT.get(Calendar.HOUR_OF_DAY));
+      System.out.println(calendarJakarta.get(Calendar.HOUR_OF_DAY)); // 6
+      System.out.println(calendarGMT.get(Calendar.HOUR_OF_DAY)); // 23
 
       calendarJakarta.setTimeZone(TimeZone.getTimeZone("GMT"));
-      System.out.println(calendarJakarta.get(Calendar.HOUR_OF_DAY));
+      System.out.println(calendarJakarta.get(Calendar.HOUR_OF_DAY)); // 23
    }
 
 }
