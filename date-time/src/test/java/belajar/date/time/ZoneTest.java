d7242fb36709a8b967acb06f53323376b2d77a1e
ltfide <lutfidendians@gmail.com>
2023-03-04 06:42:59 +0700

    ZoneId and ZoneOffset Class

--- date-time/src/test/java/belajar/date/time/ZoneTest.java
+++ date-time/src/test/java/belajar/date/time/ZoneTest.java
@@ -7,17 +7,17 @@ import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 public class ZoneTest {
-   
+
    @Test
    void createZoneId() {
       ZoneId zoneId = ZoneId.systemDefault();
-      System.out.println(zoneId);
+      System.out.println(zoneId); // Asia / Jakarta
 
       ZoneId zoneIdGMT = ZoneId.of("GMT");
-      System.out.println(zoneIdGMT);
+      System.out.println(zoneIdGMT); // GMT
 
       Set<String> availableZoneIds = ZoneId.getAvailableZoneIds();
-      availableZoneIds.forEach(id -> System.out.println(id));
+      availableZoneIds.forEach(id -> System.out.println(id)); // List of TimeZone
    }
 
    @Test
@@ -26,9 +26,9 @@ public class ZoneTest {
       ZoneOffset zoneOffset2 = ZoneOffset.ofHours(-7);
       ZoneOffset zoneOffset3 = ZoneOffset.ofHoursMinutes(3, 32);
 
-      System.out.println(zoneOffset1);
-      System.out.println(zoneOffset2);
-      System.out.println(zoneOffset3);
+      System.out.println(zoneOffset1); // +07:00
+      System.out.println(zoneOffset2); // -07:00
+      System.out.println(zoneOffset3); // +03:32
    }
 
 }
