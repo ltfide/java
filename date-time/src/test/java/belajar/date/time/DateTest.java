51e428f869e151010aa16c9663b6e96ebca25119
ltfide <lutfidendians@gmail.com>
2023-03-04 06:29:35 +0700

    Date Class

--- date-time/src/test/java/belajar/date/time/DateTest.java
+++ date-time/src/test/java/belajar/date/time/DateTest.java
@@ -12,9 +12,9 @@ public class DateTest {
         var date2 = new Date(System.currentTimeMillis());
         var date3 = new Date(1665396369000L);
 
-        System.out.println(date1);
-        System.out.println(date2);
-        System.out.println(date3);
+        System.out.println(date1); // Fri Mar 03 06:30:41 WIB 2023
+        System.out.println(date2); // Fri Mar 03 06:30:41 WIB 2023
+        System.out.println(date3); // Mon Oct 10 17:06:09 WIB 2022
     }
 
 }
