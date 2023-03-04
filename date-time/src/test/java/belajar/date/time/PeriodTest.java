51e130b8c25d9d56fa14c661dd2941a5a5fe16eb
ltfide <lutfidendians@gmail.com>
2023-03-04 08:12:23 +0700

    Period Class

--- date-time/src/test/java/belajar/date/time/PeriodTest.java
+++ date-time/src/test/java/belajar/date/time/PeriodTest.java
@@ -6,7 +6,7 @@ import java.time.Period;
 import org.junit.jupiter.api.Test;
 
 public class PeriodTest {
-   
+
    @Test
    void create() {
       Period period1 = Period.ofDays(10);
@@ -15,32 +15,31 @@ public class PeriodTest {
       Period period4 = Period.ofYears(10);
       Period period5 = Period.of(10, 10, 10);
 
-      System.out.println(period1);
-      System.out.println(period2);
-      System.out.println(period3);
-      System.out.println(period4);
-      System.out.println(period5);
+      System.out.println(period1); // P10D
+      System.out.println(period2); // P70D
+      System.out.println(period3); // P10M
+      System.out.println(period4); // P10Y
+      System.out.println(period5); // P10Y10M10D
    }
 
    @Test
    void get() {
       Period period = Period.of(10, 10, 10);
 
-      System.out.println(period.getYears());
-      System.out.println(period.getMonths());
-      System.out.println(period.getDays());
+      System.out.println(period.getYears()); // 10
+      System.out.println(period.getMonths()); // 10
+      System.out.println(period.getDays()); // 10
    }
 
    @Test
    void between() {
       Period period = Period.between(
-         LocalDate.of(1998, 5, 6),
-         LocalDate.now()
-      );
- 
-      System.out.println(period.getYears());
-      System.out.println(period.getMonths());
-      System.out.println(period.getDays());
+            LocalDate.of(1998, 5, 6),
+            LocalDate.now());
+
+      System.out.println(period.getYears()); // 24
+      System.out.println(period.getMonths()); // 9
+      System.out.println(period.getDays()); // 26
    }
 
 }
