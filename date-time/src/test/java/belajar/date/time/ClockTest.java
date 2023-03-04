36d62489e2aa737ea773e3172f165db7b216b25a
ltfide <lutfidendians@gmail.com>
2023-03-04 08:03:09 +0700

    Clock Class

--- date-time/src/test/java/belajar/date/time/ClockTest.java
+++ date-time/src/test/java/belajar/date/time/ClockTest.java
@@ -15,17 +15,17 @@ import java.time.ZonedDateTime;
 import org.junit.jupiter.api.Test;
 
 public class ClockTest {
-   
+
    @Test
    void create() {
       Clock clock1 = Clock.systemUTC();
-      System.out.println(clock1);
-  
+      System.out.println(clock1); // SystemClock[Z]
+
       Clock clock2 = Clock.systemDefaultZone();
-      System.out.println(clock2);
-  
+      System.out.println(clock2); // SystemClock[Asia/Jakarta]
+
       Clock clock3 = Clock.system(ZoneId.of("Asia/Jakarta"));
-      System.out.println(clock3);
+      System.out.println(clock3); // SystemClock[Asia/Jakarta]
    }
 
    @Test
@@ -33,21 +33,21 @@ public class ClockTest {
       Clock clock = Clock.system(ZoneId.of("Asia/Jakarta"));
 
       Instant instant1 = clock.instant();
-      System.out.println(instant1);
+      System.out.println(instant1); // 2023-03-04T00:17:56.760747200Z
 
       Thread.sleep(10_000);
 
       Instant instant2 = clock.instant();
-      System.out.println(instant2);
+      System.out.println(instant2); // 2023-03-04T00:18:06.779919700Z
    }
 
    @Test
    void fromClock() {
       Clock clock = Clock.system(ZoneId.of("Asia/Jakarta"));
-  
+
       Year year = Year.now(clock);
-      System.out.println(year);
-  
+      System.out.println(year); // 2023
+
       YearMonth yearMonth = YearMonth.now(clock);
       MonthDay monthDay = MonthDay.now(clock);
       LocalTime localTime = LocalTime.now(clock);
@@ -55,10 +55,14 @@ public class ClockTest {
       OffsetTime offsetTime = OffsetTime.now(clock);
       OffsetDateTime offsetDateTime = OffsetDateTime.now(clock);
       ZonedDateTime zonedDateTime = ZonedDateTime.now(clock);
-  
-      System.out.println(zonedDateTime);
-      System.out.println(offsetDateTime);
-      System.out.println(offsetTime);
+
+      System.out.println(yearMonth); // 2023-03
+      System.out.println(monthDay); // --03-04
+      System.out.println(localTime); // 07:18:49.572692300
+      System.out.println(localDateTime); // 2023-03-04T07:18:49.572692300
+      System.out.println(zonedDateTime); // 2023-03-04T07:18:49.578679500+07:00[Asia/Jakarta]
+      System.out.println(offsetDateTime); // 2023-03-04T07:18:49.575684200+07:00
+      System.out.println(offsetTime); // 07:18:49.573688700+07:00
    }
 
 }
--- date-time/src/test/java/belajar/date/time/InstantTest.java
+++ date-time/src/test/java/belajar/date/time/InstantTest.java
@@ -11,14 +11,14 @@ import java.time.ZonedDateTime;
 import org.junit.jupiter.api.Test;
 
 public class InstantTest {
-   
+
    @Test
    void create() {
       Instant instant1 = Instant.now();
-      System.out.println(instant1);
-  
+      System.out.println(instant1); // 2023-03-04T00:01:10.433712Z
+
       Instant instant2 = Instant.ofEpochMilli(1665468854431L);
-      System.out.println(instant2);
+      System.out.println(instant2); // 2022-10-11T06:14:14.431Z
    }
 
    @Test
@@ -26,19 +26,19 @@ public class InstantTest {
       Instant instant1 = Instant.now();
       Instant instant2 = instant1.plusMillis(1000);
       Instant instant3 = instant1.minusMillis(1000);
-  
-      System.out.println(instant1);
-      System.out.println(instant2);
-      System.out.println(instant3);
+
+      System.out.println(instant1); // 2023-03-04T00:01:56.492843300Z
+      System.out.println(instant2); // 2023-03-04T00:01:57.492843300Z
+      System.out.println(instant3); // 2023-03-04T00:01:55.492843300Z
    }
 
    @Test
    void get() {
       Instant instant = Instant.now();
 
-      System.out.println(instant.toEpochMilli());
-      System.out.println(instant.getEpochSecond());
-      System.out.println(instant.getNano());
+      System.out.println(instant.toEpochMilli()); // 1677888138385
+      System.out.println(instant.getEpochSecond()); // 1677888138
+      System.out.println(instant.getNano()); // 385971900
    }
 
    @Test
@@ -48,28 +48,28 @@ public class InstantTest {
       ZoneId zoneId = ZoneId.of("Asia/Jakarta");
 
       LocalTime localTime = LocalTime.ofInstant(instant, zoneId);
-      System.out.println(localTime);
+      System.out.println(localTime); // 07:02:39.106194600
 
       LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, zoneId);
-      System.out.println(localDateTime);
+      System.out.println(localDateTime); // 2023-03-04T07:02:39.106194600
 
       ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(instant, zoneId);
-      System.out.println(zonedDateTime);
+      System.out.println(zonedDateTime); // 2023-03-04T07:02:39.106194600+07:00[Asia/Jakarta]
 
       OffsetDateTime offsetDateTime = OffsetDateTime.ofInstant(instant, zoneId);
-      System.out.println(offsetDateTime);
+      System.out.println(offsetDateTime); // 2023-03-04T07:02:39.106194600+07:00
    }
 
    @Test
    void toInstant() {
       Instant instant1 = LocalDateTime.now().toInstant(ZoneOffset.ofHours(7));
-      System.out.println(instant1);
+      System.out.println(instant1); // 2023-03-04T00:03:23.399777500Z
 
       Instant instant2 = ZonedDateTime.now().toInstant();
-      System.out.println(instant2);
+      System.out.println(instant2); // 2023-03-04T00:03:23.422717Z
 
       Instant instant3 = OffsetDateTime.now().toInstant();
-      System.out.println(instant3);
+      System.out.println(instant3); // 2023-03-04T00:03:23.423714500Z
    }
 
 }
