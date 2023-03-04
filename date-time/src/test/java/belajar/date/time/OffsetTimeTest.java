c864cf3a1d1f22d547d90910733733370f14d8ca
ltfide <lutfidendians@gmail.com>
2023-03-04 07:00:36 +0700

    OffsetTime and OffsetDateTime class

--- date-time/src/test/java/belajar/date/time/OffsetTimeTest.java
+++ date-time/src/test/java/belajar/date/time/OffsetTimeTest.java
@@ -10,7 +10,7 @@ import java.time.ZoneOffset;
 import org.junit.jupiter.api.Test;
 
 public class OffsetTimeTest {
-   
+
    @Test
    void create() {
       OffsetTime offsetTime1 = OffsetTime.now();
@@ -18,44 +18,44 @@ public class OffsetTimeTest {
       OffsetTime offsetTime3 = OffsetTime.of(10, 10, 10, 10, ZoneOffset.ofHours(7));
       OffsetTime offsetTime4 = OffsetTime.parse("09:09:09.100+07:00");
 
-      System.out.println(offsetTime1);
-      System.out.println(offsetTime2);
-      System.out.println(offsetTime3);
-      System.out.println(offsetTime4);
+      System.out.println(offsetTime1); // 06:53:53.098980400+07:00
+      System.out.println(offsetTime2); // 06:53:53.099977500+07:00
+      System.out.println(offsetTime3); // 10:10:10.000000010+07:00
+      System.out.println(offsetTime4); // 09:09:09.100+07:00
 
       OffsetDateTime offsetDateTime1 = OffsetDateTime.now();
       OffsetDateTime offsetDateTime2 = OffsetDateTime.now(ZoneId.of("Asia/Jakarta"));
       OffsetDateTime offsetDateTime3 = OffsetDateTime.of(LocalDateTime.now(), ZoneOffset.ofHours(7));
       OffsetDateTime offsetDateTime4 = OffsetDateTime.parse("1980-09-17T10:10:10.100+05:00");
 
-      System.out.println(offsetDateTime1);
-      System.out.println(offsetDateTime2);
-      System.out.println(offsetDateTime3);
-      System.out.println(offsetDateTime4);
+      System.out.println(offsetDateTime1); // 2023-03-04T06:53:53.128899900+07:00
+      System.out.println(offsetDateTime2); // 2023-03-04T06:53:53.128899900+07:00
+      System.out.println(offsetDateTime3); // 2023-03-04T06:53:53.128899900+07:00
+      System.out.println(offsetDateTime4); // 1980-09-17T10:10:10.100+05:00
    }
 
    @Test
    void toLocalTime() {
       LocalTime localTime = LocalTime.now();
-      System.out.println(localTime);
+      System.out.println(localTime); // 06:54:39.436492100
 
       OffsetTime offsetTime = localTime.atOffset(ZoneOffset.ofHours(7));
-      System.out.println(offsetTime);
+      System.out.println(offsetTime); // 06:54:39.436492100+07:00
 
       LocalTime localTime2 = offsetTime.toLocalTime();
-      System.out.println(localTime2);
+      System.out.println(localTime2); // 06:54:39.436492100
    }
 
    @Test
    void toLocalDateTime() {
       LocalDateTime localDateTime = LocalDateTime.now();
-      System.out.println(localDateTime);
+      System.out.println(localDateTime); // 2023-03-04T06:55:04.915618400
 
       OffsetDateTime offsetDateTime = localDateTime.atOffset(ZoneOffset.ofHours(7));
-      System.out.println(offsetDateTime);
+      System.out.println(offsetDateTime); // 2023-03-04T06:55:04.915618400+07:00
 
       LocalDateTime localDateTime2 = offsetDateTime.toLocalDateTime();
-      System.out.println(localDateTime2);
+      System.out.println(localDateTime2); // 2023-03-04T06:55:04.915618400
    }
 
 }
