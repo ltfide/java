f1b8253d375c3d4368055503852f12c79476987d
ltfide <lutfidendians@gmail.com>
2023-03-04 08:18:10 +0700

    Temporal Class

--- date-time/src/test/java/belajar/date/time/TemporalTest.java
+++ date-time/src/test/java/belajar/date/time/TemporalTest.java
@@ -20,7 +20,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class TemporalTest {
-   
+
    @Test
    void temporal() {
       Temporal temporal1 = LocalDate.now();
@@ -28,10 +28,10 @@ public class TemporalTest {
       Temporal temporal3 = ZonedDateTime.now();
       Temporal temporal4 = Year.now();
 
-      System.out.println(temporal1);
-      System.out.println(temporal2);
-      System.out.println(temporal3);
-      System.out.println(temporal4);
+      System.out.println(temporal1); // 2023-03-04
+      System.out.println(temporal2); // 08:15:02.279534100
+      System.out.println(temporal3); // 2023-03-04T08:15:02.280531500+07:00[Asia/Jakarta]
+      System.out.println(temporal4); // 2023
    }
 
    @Test
@@ -40,31 +40,31 @@ public class TemporalTest {
       Temporal temporal2 = temporal1.plus(Duration.ofHours(10));
       Temporal temporal3 = temporal1.plus(Period.ofDays(10));
 
-      System.out.println(temporal1);
-      System.out.println(temporal2);
-      System.out.println(temporal3);
+      System.out.println(temporal1); // 2023-03-04T08:15:57.246266200
+      System.out.println(temporal2); // 2023-03-04T18:15:57.246266200
+      System.out.println(temporal3); // 2023-03-14T08:15:57.246266200
    }
 
    @Test
    void temporalUnit() {
       long temporalUnit1 = ChronoUnit.DAYS.between(LocalDateTime.now(), LocalDateTime.now().plusHours(10));
 
-      System.out.println(temporalUnit1);
+      System.out.println(temporalUnit1); // 0
    }
 
    @Test
    void temporalField() {
       LocalDateTime temporal = LocalDateTime.now();
 
-      System.out.println(temporal.get(ChronoField.YEAR));
-      System.out.println(temporal.get(ChronoField.MONTH_OF_YEAR));
-      System.out.println(temporal.get(ChronoField.DAY_OF_MONTH));
-      System.out.println(temporal.get(ChronoField.HOUR_OF_DAY));
-      System.out.println(temporal.get(ChronoField.MINUTE_OF_HOUR));
-      System.out.println(temporal.get(ChronoField.SECOND_OF_MINUTE));
-      System.out.println(temporal.get(ChronoField.MILLI_OF_SECOND));
-      System.out.println(temporal.get(ChronoField.MICRO_OF_SECOND));
-      System.out.println(temporal.get(ChronoField.NANO_OF_SECOND));
+      System.out.println(temporal.get(ChronoField.YEAR)); // 2023
+      System.out.println(temporal.get(ChronoField.MONTH_OF_YEAR)); // 3
+      System.out.println(temporal.get(ChronoField.DAY_OF_MONTH)); // 4
+      System.out.println(temporal.get(ChronoField.HOUR_OF_DAY)); // 8
+      System.out.println(temporal.get(ChronoField.MINUTE_OF_HOUR)); // 16
+      System.out.println(temporal.get(ChronoField.SECOND_OF_MINUTE)); // 41
+      System.out.println(temporal.get(ChronoField.MILLI_OF_SECOND)); // 897
+      System.out.println(temporal.get(ChronoField.MICRO_OF_SECOND)); // 897632
+      System.out.println(temporal.get(ChronoField.NANO_OF_SECOND)); // 897632100
    }
 
    @Test
@@ -74,40 +74,39 @@ public class TemporalTest {
       List<Integer> integers = temporal.query(new TemporalQuery<List<Integer>>() {
          @Override
          public List<Integer> queryFrom(TemporalAccessor temporal) {
-         ArrayList<Integer> list = new ArrayList<>();
+            ArrayList<Integer> list = new ArrayList<>();
 
-         list.add(temporal.get(ChronoField.YEAR));
-         list.add(temporal.get(ChronoField.MONTH_OF_YEAR));
-         list.add(temporal.get(ChronoField.DAY_OF_MONTH));
+            list.add(temporal.get(ChronoField.YEAR));
+            list.add(temporal.get(ChronoField.MONTH_OF_YEAR));
+            list.add(temporal.get(ChronoField.DAY_OF_MONTH));
 
-         return list;
+            return list;
          }
       });
 
-      System.out.println(integers);
+      System.out.println(integers); // [2023, 3, 4]
    }
 
    @Test
    void temporalAdjuster() {
       Temporal temporal1 = LocalDate.now();
-      System.out.println(temporal1);
-  
+      System.out.println(temporal1); // 2023-03-04
+
       Temporal temporal2 = temporal1.with(new TemporalAdjuster() {
-        @Override
-        public Temporal adjustInto(Temporal temporal) {
-          return LocalDate.of(
-              temporal.get(ChronoField.YEAR),
-              temporal.get(ChronoField.MONTH_OF_YEAR),
-              1
-          );
-        }
+         @Override
+         public Temporal adjustInto(Temporal temporal) {
+            return LocalDate.of(
+                  temporal.get(ChronoField.YEAR),
+                  temporal.get(ChronoField.MONTH_OF_YEAR),
+                  1);
+         }
       });
-      System.out.println(temporal2);
-  
+      System.out.println(temporal2); // 2023-03-01
+
       Temporal temporal3 = temporal1.with(TemporalAdjusters.firstDayOfMonth());
-      System.out.println(temporal3);
+      System.out.println(temporal3); // 2023-03-01
       Temporal temporal4 = temporal1.with(TemporalAdjusters.lastDayOfMonth());
-      System.out.println(temporal4);
+      System.out.println(temporal4); // 2023-03-31
    }
 
 }
