fe397041e040c2760d86292de459a49335866ec8
ltfide <lutfidendians@gmail.com>
2023-04-07 10:42:41 +0700

    person class

--- reflection/src/main/java/dev/local/Person.java
+++ reflection/src/main/java/dev/local/Person.java
@@ -1,12 +1,16 @@
 package dev.local;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
     private String firstName;
     private String lastName;
