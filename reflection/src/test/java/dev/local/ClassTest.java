ba50594c8019eda9db7623f13faf3015275b37ff
ltfide <lutfidendians@gmail.com>
2023-04-07 10:42:18 +0700

    class

--- reflection/src/test/java/dev/local/ClassTest.java
+++ reflection/src/test/java/dev/local/ClassTest.java
@@ -29,10 +29,15 @@ public class ClassTest {
         System.out.println(Arrays.toString(personClass.getDeclaredConstructors())); // [public dev.local.Person()]
 
         System.out.println(Arrays.toString(personClass.getDeclaredMethods()));
-        // [public java.lang.String dev.local.Person.toString(), public void dev.local.Person.setLastName(java.lang.String), public java.lang.String dev.local.Person.getLastName(), public void dev.local.Person.setFirstName(java.lang.String), public java.lang.String dev.local.Person.getFirstName()]
+        // [public java.lang.String dev.local.Person.toString(), public void
+        // dev.local.Person.setLastName(java.lang.String), public java.lang.String
+        // dev.local.Person.getLastName(), public void
+        // dev.local.Person.setFirstName(java.lang.String), public java.lang.String
+        // dev.local.Person.getFirstName()]
 
         System.out.println(Arrays.toString(personClass.getDeclaredFields()));
-        // [private java.lang.String dev.local.Person.firstName, private java.lang.String dev.local.Person.lastName]
+        // [private java.lang.String dev.local.Person.firstName, private
+        // java.lang.String dev.local.Person.lastName]
 
         System.out.println(personClass.getModifiers()); // 1
         System.out.println(personClass.getPackage()); // package dev.local
