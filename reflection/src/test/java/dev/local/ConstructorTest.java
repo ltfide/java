30d53648c2410564da62c90dee008b3674a45577
ltfide <lutfidendians@gmail.com>
2023-04-07 11:45:46 +0700

    constructor class

---
+++ reflection/src/test/java/dev/local/ConstructorTest.java
@@ -0,0 +1,48 @@
+package dev.local;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Parameter;
+
+import org.junit.jupiter.api.Test;
+
+public class ConstructorTest {
+
+    @Test
+    void testConstructor() {
+        Class<Person> personClass = Person.class;
+
+        Constructor<?>[] constructors = personClass.getDeclaredConstructors();
+        for (Constructor<?> constructor : constructors) {
+            System.out.println(constructor.getName());
+            Parameter[] parameters = constructor.getParameters();
+            for (Parameter parameter : parameters) {
+                System.out.println(parameter.getName());
+                System.out.println(parameter.getType());
+            }
+        }
+
+        // output
+        // dev.local.Person
+        // dev.local.Person
+        // arg0
+        // class java.lang.String
+        // arg1
+        // class java.lang.String
+    }
+
+    @Test
+    void createObjectWithConstructor() throws NoSuchMethodException, SecurityException, InstantiationException,
+            IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        Class<Person> personClass = Person.class;
+
+        Constructor<Person> emptyConstructor = personClass.getConstructor();
+        Constructor<Person> constructorParameters = personClass.getConstructor(String.class, String.class);
+
+        Person person = emptyConstructor.newInstance();
+        Person person2 = constructorParameters.newInstance("Lutfi", "Dendiansyah");
+
+        System.out.println(person); // Person(firstName=null, lastName=null)
+        System.out.println(person2); // Person(firstName=Lutfi, lastName=Dendiansyah)
+    }
+}
