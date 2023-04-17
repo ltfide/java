377603ee852c3ea71751141f6a28f931c11a67cf
ltfide <lutfidendians@gmail.com>
2023-04-17 22:20:00 +0700

    circuit breaker config

--- library/resilience4j/src/test/java/com/local/CircuitBreakerTest.java
+++ library/resilience4j/src/test/java/com/local/CircuitBreakerTest.java
@@ -3,6 +3,7 @@ package com.local;
 import org.junit.jupiter.api.Test;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -16,6 +17,26 @@ public class CircuitBreakerTest {
     @Test
     void circuitBreaker() {
         CircuitBreaker circuitBreaker = CircuitBreaker.ofDefaults("local");
+
+        for (int i = 0; i < 200; i++) {
+            try {
+                Runnable runnable = CircuitBreaker.decorateRunnable(circuitBreaker, () -> callMe());
+                runnable.run();
+            } catch (Exception e) {
+                log.error("Error : {}", e.getMessage());
+            }
+        }
+    }
+
+    @Test
+    void circuitBreakerConfig() {
+        CircuitBreakerConfig config = CircuitBreakerConfig.custom()
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                .failureRateThreshold(10f)
+                .slidingWindowSize(10)
+                .minimumNumberOfCalls(10)
+                .build();
+        CircuitBreaker circuitBreaker = CircuitBreaker.of("local", config);
 
         for (int i = 0; i < 200; i++) {
             try {
