1cdc6d3740abf522127140f0257d4336b94b4b13
ltfide <lutfidendians@gmail.com>
2023-04-17 22:37:15 +0700

    circuit breaker registry

--- library/resilience4j/src/test/java/com/local/CircuitBreakerTest.java
+++ library/resilience4j/src/test/java/com/local/CircuitBreakerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -37,6 +38,30 @@ public class CircuitBreakerTest {
                 .minimumNumberOfCalls(10)
                 .build();
         CircuitBreaker circuitBreaker = CircuitBreaker.of("local", config);
+
+        for (int i = 0; i < 200; i++) {
+            try {
+                Runnable runnable = CircuitBreaker.decorateRunnable(circuitBreaker, () -> callMe());
+                runnable.run();
+            } catch (Exception e) {
+                log.error("Error : {}", e.getMessage());
+            }
+        }
+    }
+
+    @Test
+    void circuitBreakerRegistry() {
+        CircuitBreakerConfig config = CircuitBreakerConfig.custom()
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                .failureRateThreshold(10f)
+                .slidingWindowSize(10)
+                .minimumNumberOfCalls(10)
+                .build();
+
+        CircuitBreakerRegistry registry = CircuitBreakerRegistry.ofDefaults();
+        registry.addConfiguration("config", config);
+
+        CircuitBreaker circuitBreaker = registry.circuitBreaker("local", "config");
 
         for (int i = 0; i < 200; i++) {
             try {
