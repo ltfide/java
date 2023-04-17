73bbd9ec580ab3a806f343ef7320e8e988323b45
ltfide <lutfidendians@gmail.com>
2023-04-18 05:16:40 +0700

    fallback

--- library/resilience4j/src/test/java/com/local/DecoratorsTest.java
+++ library/resilience4j/src/test/java/com/local/DecoratorsTest.java
@@ -1,6 +1,7 @@
 package com.local;
 
 import java.time.Duration;
+import java.util.function.Supplier;
 
 import org.junit.jupiter.api.Test;
 
@@ -51,5 +52,39 @@ public class DecoratorsTest {
         }
 
         Thread.sleep(10_000L);
+    }
+
+    @Test
+    void fallback() throws InterruptedException {
+        RateLimiter rateLimiter = RateLimiter.of("local-ratelimiter", RateLimiterConfig.custom()
+                .limitForPeriod(5)
+                .limitRefreshPeriod(Duration.ofMinutes(1))
+                .build());
+
+        Retry retry = Retry.of("local-retry", RetryConfig.custom()
+                .maxAttempts(10)
+                .waitDuration(Duration.ofMillis(10))
+                .build());
+
+        Supplier<String> supplier = Decorators.ofSupplier(() -> sayHello())
+                .withRetry(retry)
+                .withRateLimiter(rateLimiter)
+                .withFallback(throwable -> "Hello Guest")
+                .decorate();
+
+        System.out.println(supplier.get());
+
+        // output
+        // 05:11:41.213 [main] INFO com.local.DecoratorsTest -- Say hello
+        // 05:11:42.253 [main] INFO com.local.DecoratorsTest -- Say hello
+        // 05:11:43.265 [main] INFO com.local.DecoratorsTest -- Say hello
+        // 05:11:44.278 [main] INFO com.local.DecoratorsTest -- Say hello
+        // 05:11:45.290 [main] INFO com.local.DecoratorsTest -- Say hello
+        // 05:11:46.302 [main] INFO com.local.DecoratorsTest -- Say hello
+        // 05:11:47.314 [main] INFO com.local.DecoratorsTest -- Say hello
+        // 05:11:48.325 [main] INFO com.local.DecoratorsTest -- Say hello
+        // 05:11:49.338 [main] INFO com.local.DecoratorsTest -- Say hello
+        // 05:11:50.349 [main] INFO com.local.DecoratorsTest -- Say hello
+        // Hello Guest
     }
 }
