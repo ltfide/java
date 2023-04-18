f80bf33df27ba442b0417a91dc31846d91bd713c
ltfide <lutfidendians@gmail.com>
2023-04-18 10:54:32 +0700

    file manipulation

---
+++ io-streams/src/test/java/dev/local/FileManipulationTest.java
@@ -0,0 +1,21 @@
+package dev.local;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileManipulationTest {
+
+    @Test
+    void fileManipulation() throws IOException {
+        Path path = Path.of("newfile.txt");
+        Files.createFile(path);
+        Assertions.assertTrue(Files.exists(path));
+
+        Files.delete(path);
+        Assertions.assertFalse(Files.exists(path));
+    }
+}
