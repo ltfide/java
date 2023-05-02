01f5a785c84a0008596963bc864910bc5dbda027
ltfide <lutfidendians@gmail.com>
2023-05-02 14:50:58 +0700

    counter

---
+++ web/web-servlet/src/main/java/com/local/CounterServlet.java
@@ -0,0 +1,23 @@
+package com.local;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicLong;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/counter")
+public class CounterServlet extends HttpServlet {
+
+    private final AtomicLong counter = new AtomicLong(0);
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long total = counter.incrementAndGet();
+        String response = "Total Counter " + total;
+        resp.getWriter().println(response);
+    }
+}
