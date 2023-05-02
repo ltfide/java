09d4f93705d0c315477df67e355eb4f4882961c2
ltfide <lutfidendians@gmail.com>
2023-05-03 06:23:52 +0700

    form request

---
+++ web/web-servlet/src/main/java/com/local/FormServlet.java
@@ -0,0 +1,31 @@
+package com.local;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(urlPatterns = "/form")
+public class FormServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        InputStream input = getClass().getClassLoader().getResourceAsStream("html/form.html");
+        String html = new String(input.readAllBytes(), StandardCharsets.UTF_8);
+        resp.getWriter().println(html);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        String fullName = firstName + " " + lastName;
+        String response = "Hello " + fullName;
+        resp.getWriter().println(response);
+    }
+}
