b8cb0fd8c5a64253c83f2512f0a4c96ac7df0a09
ltfide <lutfidendians@gmail.com>
2023-05-02 14:29:11 +0700

    deleted

---
+++ web/web-servlet/src/main/java/com/local/WebServletApp.java
@@ -0,0 +1,14 @@
+package com.local;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class WebServletApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServletApp.class);
+    }
+}
