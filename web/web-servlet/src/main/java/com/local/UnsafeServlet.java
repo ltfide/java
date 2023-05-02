acd6452461b0e9d607fb5c0e1f088b9b8c43f541
ltfide <lutfidendians@gmail.com>
2023-05-02 14:46:14 +0700

    add: unsafe servlet

---
+++ web/web-servlet/src/main/java/com/local/UnsafeServlet.java
@@ -0,0 +1,24 @@
+package com.local;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/unsafe")
+public class UnsafeServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String response = req.getParameter("response");
+        try {
+            Thread.sleep(Long.parseLong(req.getParameter("sleep")));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+        resp.getWriter().println(response);
+    }
+}
