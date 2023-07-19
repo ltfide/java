d0efeedd926289a13e42fb39a5d0c02431a65cf3
ltfide <lutfidendians@gmail.com>
2023-07-19 13:34:46 +0700

    initial commit

---
+++ library/commonslang3-lib/src/main/java/com/local/commonslang3lib/Commonslang3LibApplication.java
@@ -0,0 +1,13 @@
+package com.local.commonslang3lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Commonslang3LibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Commonslang3LibApplication.class, args);
+	}
+
+}
---
+++ library/commonslang3-lib/src/test/java/com/local/commonslang3lib/Commonslang3LibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.local.commonslang3lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Commonslang3LibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
