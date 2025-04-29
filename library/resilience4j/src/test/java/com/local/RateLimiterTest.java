c9fef4dc0e04adc48437576370672fe2c4645e98
ltfide <lutfidendians@gmail.com>
2025-04-29 15:19:19 +0700

    rate limiter

--- library/resilience4j/src/test/java/com/local/RateLimiterTest.java
+++ library/resilience4j/src/test/java/com/local/RateLimiterTest.java
@@ -1,8 +1,13 @@
 package com.local;
 
 import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Supplier;
 
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
 import org.junit.jupiter.api.Test;
 
 import io.github.resilience4j.ratelimiter.RateLimiter;
@@ -38,5 +43,37 @@ public class RateLimiterTest {
             runnable.run();
         }
         // 10:26:23.556 [main] INFO com.local.RateLimiterTest -- Result 1 - 10000
+    }
+
+    @Test
+    void rateLimiter1() {
+        RateLimiterConfig config = RateLimiterConfig.custom()
+                .limitForPeriod(5)
+                .limitRefreshPeriod(Duration.ofSeconds(1))
+                .timeoutDuration(Duration.ofMillis(2000))
+                .build();
+
+        RateLimiterRegistry registry = RateLimiterRegistry.of(config);
+        RateLimiter rateLimiter = registry.rateLimiter("rateLimiter1");
+
+        Supplier<String> supplier = RateLimiter.decorateSupplier(rateLimiter, () -> {
+            System.out.println("calling external service, request: " + counter.incrementAndGet() + " time: " + LocalTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME));
+            return "success";
+        });
+
+        for (int i = 0; i < 10; i++) {
+            try {
+                String result = supplier.get();
+                //System.out.println("result: " + result);
+            } catch (RequestNotPermitted e) {
+                System.out.println("rejected by rate limiter, request: " + counter.incrementAndGet() + " time: " + LocalTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME));
+            }
+
+//            try {
+//                Thread.sleep(300);
+//            } catch (InterruptedException e) {
+//                Thread.currentThread().interrupt();
+//            }
+        }
     }
 }
