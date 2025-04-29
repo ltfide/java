51f07a40b28f3e07c592f28a8201ef0f056f476f
ltfide <lutfidendians@gmail.com>
2025-04-29 13:24:47 +0700

    circuit breaker fallback

--- library/resilience4j/src/test/java/com/local/CircuitBreakerTest.java
+++ library/resilience4j/src/test/java/com/local/CircuitBreakerTest.java
@@ -1,5 +1,8 @@
 package com.local;
 
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.event.CircuitBreakerOnCallNotPermittedEvent;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
@@ -80,7 +83,7 @@ public class CircuitBreakerTest {
 
     private String callApi() {
         System.out.println("attempting to call api" + LocalTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME));
-        if (Math.random() < 0.5) {
+        if (Math.random() < 0.8) {
             throw new RuntimeException("api is down!");
         }
         return "success";
@@ -113,6 +116,49 @@ public class CircuitBreakerTest {
 
             try {
                 Thread.sleep(1_000L);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    private String externalServiceFallback(Throwable t) {
+        System.err.println("Fallback dijalankan: " + t.getMessage());
+        return "Respons fallback: Layanan sedang bermasalah.";
+    }
+
+    @Test
+    @DisplayName("handle fallback")
+    void circuitBreaker2() {
+        CircuitBreakerConfig config = CircuitBreakerConfig.custom()
+                .failureRateThreshold(50)
+                .minimumNumberOfCalls(5)
+                .slowCallRateThreshold(100)
+                .slowCallDurationThreshold(Duration.ofSeconds(1L))
+                .waitDurationInOpenState(Duration.ofSeconds(5))
+                .permittedNumberOfCallsInHalfOpenState(2)
+                .recordExceptions(RuntimeException.class)
+                .build();
+
+        CircuitBreakerRegistry registry = CircuitBreakerRegistry.of(config);
+        CircuitBreaker circuitBreaker = registry.circuitBreaker("handleFallback");
+
+        Supplier<String> tSupplier = CircuitBreaker.decorateSupplier(circuitBreaker, this::callApi);
+
+        for (int i = 0; i < 100; i++) {
+            try {
+                String result = tSupplier.get();
+                System.out.println("Result: " + result + " (State: " + circuitBreaker.getState() + ")");
+            } catch (CallNotPermittedException e) {
+                String fallbackResult = externalServiceFallback(e);
+                System.out.println("Hasil (Fallback - Circuit Open): " + fallbackResult + " (State: " + circuitBreaker.getState() + ")");
+            } catch (RuntimeException e) {
+                String fallbackResult = externalServiceFallback(e);
+                System.out.println("Hasil (Fallback - Call Failed): " + fallbackResult + " (State: " + circuitBreaker.getState() + ")");
+            }
+
+            try {
+                Thread.sleep(1_000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
